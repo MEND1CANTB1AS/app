ef9601af3dc39f39908ca1710f025bfaf38cd1d2
Glitch (thimble-import) <none>
2020-06-25 05:00:37 +0000

    🎣📈 Checkpoint ./build-customize.html:15157524/175 ./build-customize.js:15157524/135

--- build-customize.js
+++ build-customize.js
@@ -1,15 +1,15 @@
 function setMB()
 {
     "use strict";
-    var vis = document.querySelector(".vis"),
-    target = document.getElementById(this.value);
-    if (vis !== null) 
+    var vis = document.querySelector(".vis");
+    var option = document.getElementById(this.value);
+    if (option == 0) 
     {
       vis.className = "inv";
     }
     if (target !== null) 
     {
-      target.className = "vis";
+      option.className = "vis";
     }
 }
 
