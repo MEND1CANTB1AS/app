b15c2bafc85633666827840294cfa9332ccce08f
Glitch (thimble-import) <none>
2020-06-25 05:23:36 +0000

    🏐🎹 Checkpoint ./build-customize.js:15157524/673 ./build-customize.html:15157524/79

--- build-customize.js
+++ build-customize.js
@@ -1,31 +1,21 @@
-function setMB()
+function setMB(opt0, opt1, opt2, opt3)
 {
-    "use strict";
-    var vis = document.querySelector(".vis");
-    var option = document.getElementById(this.value);
-  var opt0 = document.getElementById(mb_0);
+  var vis = document.querySelector(".vis");
+
+  
+  /*var opt0 = document.getElementById(mb_0);
   var opt1 = document.getElementById(mb_1);
   var opt2 = document.getElementById(mb_2);
-  var opt3 = document.getElementById(mb_3);
+  var opt3 = document.getElementById(mb_3);*/
   
   opt0.className = "inv";
   opt1.className = "inv";
   opt2.className = "inv";
   opt3.className = "inv";
   
-  option
-    if (option == "mb_0") 
-    {
-      vis.className = "vis";
-      opt0.className = "inv";
-      opt0.className = "inv";
-      opt0.className = "inv";
-      opt0.className = "inv";
-    }
-    if (target !== null) 
-    {
-      option.className = "vis";
-    }
+  var option = document.getElementById(op);
+  option.className = "vis";
+
 }
 
 function setCPU()
